7e0e4b90a2d172d10af92d59751d17b13c718be9
justinfinn2891 <jfinn5@ut.utm.edu>
2024-12-20 22:32:29 -0600

    html+css+javascript update

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 
 
-let pullup, pushupWeight, pushup, curl;
-
+let pullup, pushupWeight, pushup, curl, XP;
+let totalXP = 0;
+let level = 0; 
+let nextXP = 500;
 document.getElementById("submit").onclick = function()
 {
     pushup = (document.getElementById("pushup").value) * 2;
-    pushupWeight = (document.getElementById("pushupWeight").value) *2;
     pullup = (document.getElementById("pullup").value) * 2;
     curl = (document.getElementById("curl").value) * 2;
-    curlWeight = (document.getElementById("curlWeight").value) *4;
 
-    totalXP = pullup + pushupWeight + pushup + curl; 
+
+    XP = (pullup + pushup + curl);
+    totalXP += XP; 
+    document.getElementById("xp").textContent = `Total XP: ${totalXP}`;
+    console.log(totalXP)
+    if(totalXP > nextXP)
+    {
+        console.log("Done");
+        level+=1;
+        console.log(level);
+        nextXP = nextXP + 500;
+    }
+    console.log(nextXP);
+    console.log(level);
+    document.getElementById("level").textContent = `Level: ${level}`;
 }
 
 /*
